e9a182c8355b791cca4754178ef076450ac3534f
niduzi <ahmadizzuddin@protonmail.com>
2020-11-23 18:21:27 +0700

    ncurses input test

--- bin/main.c
+++ bin/main.c
@@ -2,13 +2,18 @@
 
 int main()
 {
+    char str[256] = "";
     initscr();
     cbreak();
-    noecho();
     keypad(stdscr, TRUE);
     WINDOW *win = newwin(LINES, COLS, 0, 0);
     waddstr(win, "-----------------\n|    Arrozy   |\n| ncurses Demo  |\n-----------------\n\n");
+    waddstr(win, "\nType in the string you want...\n");
     wrefresh(win);
+    wgetnstr(win, str, 255);
+    wmove(win, 0,0);
+    wclear(win);
+    waddstr(win, str);
     waddstr(win, "\npress any key to exit...");
     wrefresh(win);
     wgetch(win);
