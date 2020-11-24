5ac032d233efdeb2102162924689c88bf0154964
nidduzzi <74892632+nidduzzi@users.noreply.github.com>
2020-11-24 13:59:51 +0700

    zoom meeting

--- bin/main.c
+++ bin/main.c
@@ -5,19 +5,73 @@ int main()
     char str[256] = "";
     initscr();
     cbreak();
-    keypad(stdscr, TRUE);
-    WINDOW *win = newwin(LINES, COLS, 0, 0);
-    waddstr(win, "-----------------\n|    Arrozy   |\n| ncurses Demo  |\n-----------------\n\n");
-    waddstr(win, "\nType in the string you want...\n");
-    wrefresh(win);
-    wgetnstr(win, str, 255);
-    wmove(win, 0,0);
-    wclear(win);
-    waddstr(win, str);
-    waddstr(win, "\npress any key to exit...");
-    wrefresh(win);
-    wgetch(win);
-    delwin(win);
+    // noecho();
+    if (has_colors())
+    {
+        if(LINES > 50 || COLS > 1000)
+        {
+            addstr("Terminal too small\n");
+            refresh();
+            getch();
+        }
+        else
+        {
+
+            if (start_color() == OK)
+            {
+                init_pair(1, COLOR_YELLOW, COLOR_RED);
+                init_pair(2, COLOR_GREEN, COLOR_GREEN);
+                init_pair(3, COLOR_MAGENTA, COLOR_CYAN);
+
+                attrset(COLOR_PAIR(1));
+                addstr("Yellow and red\n\n");
+                refresh();
+                attroff(COLOR_PAIR(1));
+
+                attrset(COLOR_PAIR(2) | A_BOLD);
+                addstr("Green and green A_BOLD\n\n");
+                refresh();
+                attroff(COLOR_PAIR(2));
+                attroff(A_BOLD);
+
+                attrset(COLOR_PAIR(3));
+                addstr("Magenta and cyan\n");
+                refresh();
+                addstr("press any key for new window...");
+                attroff(COLOR_PAIR(3));
+                wgetch(stdscr);
+
+                WINDOW *win = newwin(LINES, COLS, 0, 0);
+                keypad(win, false);
+                wclear(win);
+                wattrset(win, COLOR_PAIR(1));
+                sprintf(str, "-----------------\n|     Arrozy    |\n| ncurses Demo %d|\n-----------------\n\n", 1);
+                waddstr(win, str);
+                waddstr(win, "\nType any string you want...\n");
+                wrefresh(win);
+                wgetnstr(win, str, 255);
+                wmove(win, 10, 10);
+                
+                waddstr(win, str);
+                waddstr(win, "\npress any key to exit...");
+                wrefresh(win);
+                wgetch(win);
+                wattroff(win, COLOR_PAIR(1));
+                delwin(win);
+            }
+            else
+            {
+                addstr("Cannot start colours\n");
+                refresh();
+            }
+        }
+    }
+    else
+    {
+        addstr("Not colour capable\n");
+        refresh();
+    }
+
     endwin();
     return EXIT_SUCCESS;
 }
