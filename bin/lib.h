9cb19f85c7fec03a1d4194507a7dddb36b5b2eb5
nidduzzi <74892632+nidduzzi@users.noreply.github.com>
2020-11-24 13:59:51 +0700

    diskusi zoom

--- bin/lib.h
+++ bin/lib.h
@@ -14,7 +14,8 @@ typedef enum game_internal_state_t
     GAME_INIT,    // Initialize level assets(bg, level design, sprites, prng seed, etc)
     BULLET_READY, // Bullet has been initialized (color/type set) and waiting for player to aim and shoot bullet --> update state to BULLET_FIRED after player fires bullet
     BULLET_FIRED, /* Player has set bullet angle and fired the bullet --> update positon and reflect angle if barrier is hit --> update state to BULLET_HIT when distance to any bubble is less than update displacement*/
-    BULLET_HIT    /* Bullet has hit a bubble --> comprison of bullet and bubble color --> destroy bubbles if 3 same color are adjacent --> destroy bubbles not connected to original layout --> Count score --> rng get next bullet type --> update state to BULLET_READY*/
+    BULLET_HIT,   /* Bullet has hit a bubble --> comprison of bullet and bubble color --> destroy bubbles if 3 same color are adjacent --> destroy bubbles not connected to original layout --> Count score --> rng get next bullet type --> update state to BULLET_READY*/
+    GAME_END
 } g_is_t;
 
 typedef struct assetF_addr_t
--- bin/main.c
+++ bin/main.c
@@ -5,53 +5,65 @@ int main()
     char str[256] = "";
     initscr();
     cbreak();
+    // noecho();
     if (has_colors())
     {
-        if (start_color() == OK)
+        if(LINES > 50 || COLS > 1000)
         {
-            init_pair(1, COLOR_YELLOW, COLOR_RED);
-            init_pair(2, COLOR_GREEN, COLOR_GREEN);
-            init_pair(3, COLOR_MAGENTA, COLOR_CYAN);
-
-            attrset(COLOR_PAIR(1));
-            addstr("Yellow and red\n\n");
+            addstr("Terminal too small\n");
             refresh();
-            attroff(COLOR_PAIR(1));
-
-            attrset(COLOR_PAIR(2) | A_BOLD);
-            addstr("Green and green A_BOLD\n\n");
-            refresh();
-            attroff(COLOR_PAIR(2));
-            attroff(A_BOLD);
-
-            attrset(COLOR_PAIR(3));
-            addstr("Magenta and cyan\n");
-            refresh();
-            addstr("press any key for new window...");
-            attroff(COLOR_PAIR(3));
             getch();
-
-            WINDOW *win = newwin(LINES, COLS, 0, 0);
-            keypad(win, TRUE);
-            wattrset(win, COLOR_PAIR(1));
-            sprintf(str, "-----------------\n|     Arrozy    |\n| ncurses Demo %d|\n-----------------\n\n", 1);
-            waddstr(win, str);
-            waddstr(win, "\nType any string you want...\n");
-            wrefresh(win);
-            wgetnstr(win, str, 255);
-            wmove(win, 0, 0);
-            wclear(win);
-            waddstr(win, str);
-            waddstr(win, "\npress any key to exit...");
-            wrefresh(win);
-            wgetch(win);
-            wattroff(win, COLOR_PAIR(1));
-            delwin(win);
         }
         else
         {
-            addstr("Cannot start colours\n");
-            refresh();
+
+            if (start_color() == OK)
+            {
+                init_pair(1, COLOR_YELLOW, COLOR_RED);
+                init_pair(2, COLOR_GREEN, COLOR_GREEN);
+                init_pair(3, COLOR_MAGENTA, COLOR_CYAN);
+
+                attrset(COLOR_PAIR(1));
+                addstr("Yellow and red\n\n");
+                refresh();
+                attroff(COLOR_PAIR(1));
+
+                attrset(COLOR_PAIR(2) | A_BOLD);
+                addstr("Green and green A_BOLD\n\n");
+                refresh();
+                attroff(COLOR_PAIR(2));
+                attroff(A_BOLD);
+
+                attrset(COLOR_PAIR(3));
+                addstr("Magenta and cyan\n");
+                refresh();
+                addstr("press any key for new window...");
+                attroff(COLOR_PAIR(3));
+                wgetch(stdscr);
+
+                WINDOW *win = newwin(LINES, COLS, 0, 0);
+                keypad(win, false);
+                wclear(win);
+                wattrset(win, COLOR_PAIR(1));
+                sprintf(str, "-----------------\n|     Arrozy    |\n| ncurses Demo %d|\n-----------------\n\n", 1);
+                waddstr(win, str);
+                waddstr(win, "\nType any string you want...\n");
+                wrefresh(win);
+                wgetnstr(win, str, 255);
+                wmove(win, 10, 10);
+                
+                waddstr(win, str);
+                waddstr(win, "\npress any key to exit...");
+                wrefresh(win);
+                wgetch(win);
+                wattroff(win, COLOR_PAIR(1));
+                delwin(win);
+            }
+            else
+            {
+                addstr("Cannot start colours\n");
+                refresh();
+            }
         }
     }
     else
