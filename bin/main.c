f6aa4bb268fd205a24e89ac67fb14a0f331a54bd
nidduzzi <74892632+nidduzzi@users.noreply.github.com>
2020-11-26 11:24:23 +0700

    cursor moving & print content of file at cursor position

--- bin/main.c
+++ bin/main.c
@@ -8,7 +8,7 @@ int main()
     // noecho();
     if (has_colors())
     {
-        if(LINES > 50 || COLS > 1000)
+        if (LINES > 50 || COLS > 1000)
         {
             addstr("Terminal too small\n");
             refresh();
@@ -20,7 +20,7 @@ int main()
             if (start_color() == OK)
             {
                 init_pair(1, COLOR_YELLOW, COLOR_RED);
-                init_pair(2, COLOR_GREEN, COLOR_GREEN);
+                init_pair(2, COLOR_BLACK, COLOR_GREEN);
                 init_pair(3, COLOR_MAGENTA, COLOR_CYAN);
 
                 attrset(COLOR_PAIR(1));
@@ -42,7 +42,7 @@ int main()
                 wgetch(stdscr);
 
                 WINDOW *win = newwin(LINES, COLS, 0, 0);
-                keypad(win, false);
+                keypad(win, true);
                 wclear(win);
                 wattrset(win, COLOR_PAIR(1));
                 sprintf(str, "-----------------\n|     -test-    |\n| ncurses Demo %d|\n-----------------\n\n", 1);
@@ -52,16 +52,55 @@ int main()
                 wgetnstr(win, str, 255);
                 wmove(win, 0, 0);
                 strcat(str, "\n");
+                wclear(win);
                 waddstr(win, str);
+                waddstr(win, "move your cursor to determine where to paste the incoming file...(press enter to continue)\n");
+                int row = 2, col = 0, key = 0;
+                noecho();
+                while ((key = wgetch(win)) != (int)'\n')
+                {
+                    switch (key)
+                    {
+                    case KEY_UP:
+                        row -= (row > 1) ? 1 : 0;
+                        break;
+                    case KEY_DOWN:
+                        row += (row < LINES) ? 1 : 0;
+                        break;
+                    case KEY_LEFT:
+                        col -= (col >= 0) ? 1 : 0;
+                        break;
+                    case KEY_RIGHT:
+                        col += (col < COLS) ? 1 : 0;
+                        break;
+                    case KEY_ENTER:
+                        return 0;
+                        break;
+                    default:
+                        break;
+                    }
+                    wmove(win, row, col);
+                    addch((char)219);
+                    wrefresh(win);
+                    wdelch(win);
+                }
+                wattroff(win, COLOR_PAIR(1));
+                wattron(win, COLOR_PAIR(2));
                 int maxline = 0, maxrow = 0, i;
                 char **tmp;
                 textLoader("../data/l1/level.txt", &tmp, &maxrow, &maxline);
-                for(i = 0; i < maxline; ++i) waddstr(win, tmp[i]);
-
+                for (i = 0; i < maxline; ++i)
+                {
+                    wmove(win, row + i, col);
+                    waddstr(win, tmp[i]);
+                }
+                wattroff(win, COLOR_PAIR(2));
+                wattron(win, COLOR_PAIR(3));
+                wmove(win, row + maxline, 0);
                 waddstr(win, "\npress any key to exit...");
                 wrefresh(win);
+                wattroff(win, COLOR_PAIR(3));
                 wgetch(win);
-                wattroff(win, COLOR_PAIR(1));
                 delwin(win);
             }
             else
