53a21c8d5f27fbec4dc7b02415900537c8311eb8
Sijull <fikaryunara@gmail.com>
2020-12-01 07:34:55 +0700

    Contoh menu game

--- bin/main.c
+++ bin/main.c
@@ -6,8 +6,60 @@ int main()
     setlocale(LC_ALL, "");
     game_loop(1);
     initscr();
+    noecho();
     cbreak();
-    addstr("Test ncurses");
+    curs_set(0);
+    //addstr("Test ncurses");
+
+    // set x and y max value
+    int yMax;
+    int xMax;
+    getmaxyx(stdscr, yMax, xMax);
+
+    //create window
+    WINDOW *win = newwin(20,50,yMax/4,xMax/4);
+    box(win,0,0);
+    refresh();
+    wrefresh(win);
+    keypad(win,true);
+
+    char menu[3][50] = {"1-PLAY","2-SELECT_LEVEL","3-QUIT"};
+    int choice;
+    int highlight = 0;
+    
+    mvwprintw(win, 5, 10, "WELCOME TO CLI BUBBLE SHOOTER");
+    mvwprintw(win, 6, 10, "=============================");
+    
+    while(1){
+        for(int i = 0 ; i < 3 ; i++){
+            if(i == highlight){
+                wattron(win,A_REVERSE);
+            }
+            mvwprintw(win,i+9,15,menu[i]);
+            wattroff(win,A_REVERSE);
+        }
+        choice = wgetch(win);
+        switch(choice){
+            case KEY_UP:
+                highlight--;
+                if(highlight == -1){
+                    highlight = 0;
+                }
+                break;
+            case KEY_DOWN:
+                highlight++;
+                if(highlight == 3){
+                    highlight = 2;
+                }
+                break;
+            default:
+                break;
+        }
+        if(choice == 10){
+            break;
+        }
+    }
+    printw("Your choices : %s", menu[highlight]);
     getch();
     /*
     // noecho();
